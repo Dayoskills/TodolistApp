81198e30bc45e3e36a94f465116c21f79f9f1c36
Muser-am <adamamusa32@gmail.com>
2023-03-09 21:19:35 +0100

    added files

---
+++ update!!.php
@@ -0,0 +1,8 @@
+<?php
+include ("connection.php");
+$List = $_POST['list'];
+$id = $_POST['id'];
+$List = $_POST['list'];
+mysqli_query($conn, "UPDATE `tabletodo` SET `list`='$List' WHERE id = '$id'");
+header("location: index.php");
+?>
