89f0b0c1b8b44407b3d56b5f095ace4354816420
Muser-am <adamamusa32@gmail.com>
2023-03-10 01:25:44 +0100

    bug fixed

--- update_one.php
+++ update_one.php
@@ -9,22 +9,23 @@
    
 </head>
 <?php
-$id = $_GET['id'];
 include ("connection.php");
-$Rdata = mysqli_query($conn, "select * from tabletodo where id = $id");
+$id = $_GET['update'];
+$Rdata = mysqli_query($conn, "select * from tasks where id = $id");
 $data = mysqli_fetch_array($Rdata);
 ?>
 <body class="bg-info">
-<form action="update_one.php" method="POST">
+<form action="update_two.php" method="POST">
     <div class="container">
         <div class="row justify-content-center m-auto shadow bg-white mt-3 py-3 col-md-6">
             <h3 class="text-center text-primary font-monospace">Update</h3>  
             <div class="col-10">
-                <input type="text" required value="<?php echo $data['list']?>" name="list" class="form-control">
+                <input type="text" required value="<?php echo $data['name']?>" name="name" class="form-control">
             </div>  
             <div class="col-2">
                 <button class="btn btn-outline-primary">done</button>
                 <input type="hidden" name="id" value="<?php echo $data['id']?>">
+                <input type="hidden" value="<?php echo $data['name']?>">
             </div>
         </div>
     </div>
--- update_two.php
+++ update_two.php
@@ -1,9 +1,7 @@
-<!--connection-->
 <?php
 include ("connection.php");
-$List = $_POST['list'];
 $id = $_POST['id'];
-$List = $_POST['list'];
-mysqli_query($conn, "UPDATE `tabletodo` SET `list`='$List' WHERE id = '$id'");
+$name = $_POST['name'];
+mysqli_query($conn, "UPDATE `tasks` SET `name`='$name' WHERE id = '$id'");
 header("location: index.php");
 ?>
